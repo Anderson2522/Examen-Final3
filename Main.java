478c624cc96d5ea99f01d96372e0cd85a39830e2
Anderson <anderson@localhost.localdomain>
2025-09-27 08:40:43 -0600

    Se arregla codigo

--- Main.java
+++ Main.java
@@ -1,10 +1,8 @@
-import javax.swing.*;
 import vistas.VistaInicio;
 
 public class Main {
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
             new VistaInicio(); 
-        });
+        }
     }
-}
+
